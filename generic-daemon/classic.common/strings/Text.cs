6ac4969bbcedbdc16a2d2e08430791530f11c5d1
Jan-Olof Nilsson <jan-olof.nilsson@afconsult.com>
2020-11-16 15:27:48 +0100

    More command things.

---
+++ generic-daemon/classic.common/strings/Text.cs
@@ -0,0 +1,45 @@
+﻿using classic.common.errors;
+using LaYumba.Functional;
+using static LaYumba.Functional.F;
+
+namespace classic.common.strings
+{
+    /// <summary>
+    /// A string that contains something.
+    /// </summary>
+    public struct Text
+    {
+        private Text(string value) => Value = value;
+
+        private string Value { get; }
+
+        public static implicit operator string(Text c) =>
+            c.Value;
+
+        public static bool operator !=(Text left, Text right) =>
+            left.Value != right.Value;
+
+        public static bool operator ==(Text left, Text right) =>
+            left.Value.Equals(right.Value);
+
+        public override bool Equals(object? obj) =>
+            obj is Text ft && this == ft;
+
+        public override int GetHashCode() =>
+            Value.GetHashCode();
+
+        /// <inheritdoc/>
+        public override string ToString() =>
+            Value;
+
+        public static Option<Text> Create(string name) =>
+            string.IsNullOrWhiteSpace(name)
+                ? None
+                : Some(new Text(name));
+
+        public static Validation<Text> CreateAndValidate(string name, string origin) =>
+            string.IsNullOrWhiteSpace(name)
+                ? Invalid(ErrorFactory.Missing($"Name: {name}", nameof(Text), origin))
+                : Valid(new Text(name));
+    }
+}
